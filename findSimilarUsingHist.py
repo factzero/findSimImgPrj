dc2ba5216a22c4eee352d4524c534c0847509649
factzero <azlinzhi@aliyun.com>
2016-01-09 02:17:40 +0800

    同时显示原图和RGB直方图

--- findSimilarUsingHist.py
+++ findSimilarUsingHist.py
@@ -31,17 +31,21 @@ if __name__ == '__main__':
     uInPath = unicode(InPath, 'utf8')
     ShowW = 256
     ShowH = 256
-    cv2.namedWindow('Img', cv2.WINDOW_AUTOSIZE)
+    cv2.namedWindow('ImgAll', cv2.WINDOW_AUTOSIZE)
+    ImgAll = np.zeros((ShowH*2, ShowW*2, 3), np.uint8)
     for filename in os.listdir(uInPath):
         Fullfile = os.path.join(uInPath, filename)
         print Fullfile
         Fullfile = Fullfile.decode('utf8').encode('gbk')
-        cvImgOri = cv2.imread(Fullfile)
+        cvImgOri = cv2.imread(Fullfile, cv2.IMREAD_COLOR)
         cvImgShow = cv2.resize(cvImgOri, (ShowW, ShowH))
         HistRGB = calcHistAndNorm(cvImgShow)
+        ImgAll[0:ShowH, 0:ShowW, 0:3] = cvImgShow.copy()
+        ImgAll[0:ShowH, ShowW:ShowW*2, 0:3] = HistRGB.copy()
+        cv2.putText(ImgAll, 'SRC', (0, 16), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), thickness = 1)
+        cv2.putText(ImgAll, 'RGB Hist', (ShowW, 16), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), thickness = 1)
 
-        cv2.imshow('Img', cvImgShow)
-        cv2.imshow('ImgHist', HistRGB)
-        cv2.waitKey(500)
+        cv2.imshow('ImgAll', ImgAll)
+        cv2.waitKey(300)
 
     cv2.destroyAllWindows()
